1f0c6e446e8596947e7025633820d8255ed66a67
parkerappwrk <parker.s@appwrk.com>
2023-08-07 11:20:41 +0530

    aside json list

---
+++ src/data/AsideList.js
@@ -0,0 +1,42 @@
+export const AsideList = [
+  {
+    name: "Home",
+    icon: "icon-home",
+    link: "",
+  },
+  {
+    name: "Upline WhatsApp Number",
+    icon: "icon-whatsapp-line",
+    link: "menu/upline-whatsapp-number",
+  },
+  {
+    name: "Balance Overview",
+    icon: "icon-wallet",
+    link: "menu/balance-overview",
+  },
+  {
+    name: "Current Bets",
+    icon: "icon-list",
+    link: "menu/current-bets",
+  },
+  {
+    name: "Bets History",
+    icon: "icon-history",
+    link: "menu/bets-history",
+  },
+  {
+    name: "Profit & Loss",
+    icon: "icon-profit-loss",
+    link: "menu/profit-and-loss",
+  },
+  {
+    name: "Active Log",
+    icon: "icon-active-log",
+    link: "menu/active-log",
+  },
+  {
+    name: "My Profile",
+    icon: "icon-profile",
+    link: "menu/my-profile",
+  },
+];
